5832ee1ee1d58aa7a011721d6b16a0746c966b5a
Виктор Гвоздиков <starosta-91@ya.ru>
2019-12-16 11:47:00 +0300

    first auth functions

--- example/01_connect.php
+++ example/01_connect.php
@@ -1,10 +1,11 @@
 <?
 
 include_once "../vendor/autoload.php";
-$domain  = 'julliflo';
-$account = 'julli.flo@yandex.ru';
-$token   = '5d1ebdb131b08a44cdfc7f5a63f860fe1d9c182d';
 
+$domain  = '';
+$token  = '';
 
+include_once "test_access.php";
 
-$amo = new \ApiClub\AmoCRM($domain,$account,$token);
+$amo = new \ApiClub\AmoCRM($domain,$token);
+$amo->auth();
--- src/AmoCRM.php
+++ src/AmoCRM.php
@@ -8,28 +8,86 @@ class AmoCRM {
     var $token = '';
     /** @var string поддомен пользователя */
     var $domain = '';
-    /** @var string аккаунт пользователя, от имени которого подлюкчаемся */
-    var $account = '';
 
     /**
      * AmoCRM constructor.
      * @param string $domain - домен пользователя
-     * @param string $account - аккаунт от имени которого подключаемся
      * @param string $token - токен аккаунта для работы с api
-     * @throws Exceptions
      */
-    public function __construct(string $domain,string $account,string $token) {
+    public function __construct(string $domain='',string $token='') {
         $domain        = trim($domain);
-        $account       = trim($account);
         $token         = trim($token);
 
-        if(!$domain)  throw new Exceptions(Exceptions::EMPTY_DOMAIN);
-        if(!$account) throw new Exceptions(Exceptions::EMPTY_ACCOUNT);
-        if(!$token)   throw new Exceptions(Exceptions::EMPTY_TOKEN);
-
         $this->domain  = $domain;
-        $this->account = $account;
         $this->token   = $token;
+    }
+
+
+    function auth(){
+        return $this->request('api/v2/account');
+    }
+
+    function access_token($client_id,$client_secret,$code){
+
+        $data = [
+            'client_id'     => $client_id,
+            'client_secret' => $client_secret,
+            'grant_type'    => 'authorization_code',
+            'code'          => $code,
+            'redirect_uri'  => 'https://api-club.ru/amo.php',
+        ];
+        $this->request('oauth2/access_token',$data,'POST');
+    }
+
+
+    protected function link($link = ''){
+        return 'https://'.$this->domain.'.amocrm.ru/'.$link;
+    }
+
+    protected function request($link,$data=[],$custom_request="GET"){
+        return self::_request($this->domain,$this->token,$this->link($link),$data,$custom_request);
+    }
+
+    static function _request($domain,$token,$link,$data = [],$custom_request="GET"){
+        $headers = [];
+        if($token) {
+            $headers = ['Authorization: Bearer ' . $token];
+        }
+        if(!$domain)  trigger_error(Exceptions::EMPTY_DOMAIN, E_USER_NOTICE);
+
+        $curl = curl_init(); #Сохраняем дескриптор сеанса cURL
+        #Устанавливаем необходимые опции для сеанса cURL
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
+        curl_setopt($curl, CURLOPT_URL, $link);
+        curl_setopt($curl,CURLOPT_HTTPHEADER, $headers);
+        if ($custom_request) curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $custom_request);
+        if ($data) curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_COOKIEFILE, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+        curl_setopt($curl, CURLOPT_COOKIEJAR, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+
+
+        $out = curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
+
+
+        echo '<pre>';
+        echo var_dump($out);
+        echo '</pre>';
+
+        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE); #Получим HTTP-код ответа сервера
+        curl_close($curl); #Завершаем сеанс cURL
+        $out = json_decode($out);
+
+        echo '<pre>';
+        echo var_dump($out);
+        echo '</pre>';
+
+        return $out;
+
     }
 
 }
